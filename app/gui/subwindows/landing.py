c79951b5a8f3d57b53bc9acf54fc6ab98c944b23
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-05-12 12:32:29 -0400

    BaseLink

--- app/gui/subwindows/landing.py
+++ app/gui/subwindows/landing.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import font
+from tkinter import font, ttk
 try:
     from ctypes import windll
     windll.shcore.SetProcessDpiAwareness(1)
@@ -7,9 +7,13 @@ except ImportError:
     print("Error: windll not imported. Text may be blurred")
     pass
 
+from util.model import Model
+from config import CONFIG_JSON_PATH
+
 BUTTON_WIDTH = 30
 BUTTON_HEIGHT = 3
 BUTTON_PADDING = 15
+EDIT_BTN_PADX_RIGHT = 225
 
 
 
@@ -20,7 +24,7 @@ class Landing(tk.Frame):
 
         #GROUPER
         self.body = tk.Frame(self, bg="red")
-        self.body.pack( expand=True,padx=10, pady=10)
+        self.body.pack( expand=True,padx=40, ipady=10, fill='x')
         
         #BODY
         new_post_btn = tk.Button(
@@ -58,78 +62,65 @@ class Landing(tk.Frame):
         pass
 
 class BaseLink(tk.Frame):
+    
     def __init__(self, container):
         super().__init__(container)
         FONT = font.Font(family="Helvetica", size=20, weight="bold")
+        self.config_data = None
+        self.base_link = None
         self.base_link_var = tk.StringVar()
 
         #GROUPER
         self.body = tk.Frame(self)
-        self.body.columnconfigure(0,weight=1)
-        self.body.columnconfigure(1, weight=1)
+        self.body.columnconfigure(0, weight=1)
+        self.body.columnconfigure(1, weight=3)
         self.body.columnconfigure(2, weight=1)
         self.body.columnconfigure(3, weight=1)
-        self.body.pack()
+        self.body.pack(fill='x')
 
         #BODY
         self.lbl = tk.Label(self.body, text="Base Link:", font=FONT)
         self.lbl.grid(column=0, row=0)
 
-        self.text_field = tk.Text(
+        self.text_field = ttk.Entry(
             master=self.body, 
-            width= 30,
-            height= 1,
-            wrap="word",
+            width= 40,
             font=FONT,
-            padx=2,
-            pady=2,
             state="disabled")
         self.text_field.grid(column=1,row=0)
 
-        self.edit_btn = tk.Button(self.body, text="Edit", font=FONT,command= self.edit)
-        self.edit_btn.grid(column=2,row=0)
-
+        self.edit_btn = tk.Button(self.body, text="Edit", font=FONT, command=self.edit)
+        self.edit_btn.grid(column=2, row=0, padx=(0, EDIT_BTN_PADX_RIGHT))  
         self.update_btn = tk.Button(self.body, text="Update", font=FONT, command=self.update)
         self.cancel_btn = tk.Button(self.body, text="Cancel", font=FONT, command=self.cancel)
+        self.load_config_base_link()
 
     def edit(self):
         self.edit_btn.grid_forget()
         self.update_btn.grid(column=2, row=0)
         self.cancel_btn.grid(column=3, row=0)
-        pass
-
+        self.text_field.config(state="normal")
 
     def update(self):
         self.update_btn.grid_forget()
         self.cancel_btn.grid_forget()
-        self.edit_btn.grid(column=2, row=0)
-        pass
+        self.edit_btn.grid(column=2, row=0, padx=(0, EDIT_BTN_PADX_RIGHT))
+        self.text_field.config(state="disabled")
+        #TODO UPDATE JSON
+        self.load_config_base_link()
 
     def cancel(self):
         self.update_btn.grid_forget()
         self.cancel_btn.grid_forget()
-        self.edit_btn.grid(column=2, row=0)
+        self.edit_btn.grid(column=2, row=0, padx=(0, EDIT_BTN_PADX_RIGHT))
+        self.text_field.config(state="disabled")
+        self.load_config_base_link()
 
-
-
-
-
-    def switch_edit_update(self):
-        if self.btn_state == "edit":
-            self.btn_state == "update"
-        elif self.btn_state == "update":
-            #update json
-            self.btn_state == "edit"
-        self.edit_update()
-    
-    def edit_update(self):
-        if self.btn_state == "edit":
-            self.text_field.config(state="disabled")
-            self.edit_update_btn.config(text="Edit")
-            self.cancel_btn.grid_forget()
-            pass
-        elif self.btn_state == "update":
-            self.text_field.config(state="normal")
-            self.edit_update_btn.config(text="Update")
-            self.cancel_btn.grid(column=3,row=0)
-            pass
+    def load_config_base_link(self):
+        self.config_data = Model.open_json(CONFIG_JSON_PATH)
+        self.base_link = self.config_data["base_link"]
+        self.text_field.config(state="normal")
+        self.text_field.delete(0, tk.END)
+        self.text_field.insert(0, self.base_link)
+        self.text_field.config(state="disabled")
+        pass
