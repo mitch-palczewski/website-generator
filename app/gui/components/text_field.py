d6d6405827eaadb24c7e275d47cfc2a43efca037
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-05-12 10:17:02 -0400

    Enable post messaging feature and refactor messaging configuration UI

--- app/gui/components/text_field.py
+++ app/gui/components/text_field.py
@@ -9,7 +9,7 @@ except ImportError:
 
 
 class TextField(tk.Frame):
-    def __init__(self, container, feild_height):
+    def __init__(self, container, field_height):
         super().__init__(container)
         self.main_frame = tk.Frame(self, bg="blue")
         self.main_frame.pack(padx=10, pady=10)
@@ -17,7 +17,7 @@ class TextField(tk.Frame):
         self.text_field = tk.Text(
             master=self.main_frame, 
             width=50,
-            height= feild_height,
+            height= field_height,
             wrap="word",
             padx=10,
             pady=4)
@@ -25,5 +25,7 @@ class TextField(tk.Frame):
 
     def get_text(self):
         raw_text = self.text_field.get("1.0", tk.END)
-        #may need format text better
         return raw_text
+    
+    def set_text(self, text:str):
+        self.text_field.insert("1.0", text)
--- app/gui/subwindows/configure_windows/general_configuration.py
+++ app/gui/subwindows/configure_windows/general_configuration.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-import shutil
-import os
+import re
 from bs4 import BeautifulSoup as bs
+from tkinter.messagebox import showinfo, askokcancel
 try:
     from ctypes import windll
     windll.shcore.SetProcessDpiAwareness(1)
@@ -11,6 +11,7 @@ except ImportError:
 
 from util.model import Model
 from config import CONFIG_JSON_PATH
+from gui.components.text_field import TextField
 
 class GeneralConfiguration(tk.Frame):
     def __init__(self, container):
@@ -22,10 +23,10 @@ class GeneralConfiguration(tk.Frame):
         main_frame.columnconfigure(1, weight=1)
         main_frame.pack(fill='both')
         left_frame = tk.Frame(main_frame )
-        left_frame.grid(column=0, row=0)
+        left_frame.grid(column=0, row=0, sticky=tk.NSEW)
         
         messaging_frame = ConfigureMessaging(left_frame)
-        messaging_frame.pack()
+        messaging_frame.pack(pady=10)
         
         
 
@@ -36,8 +37,13 @@ class ConfigureMessaging(tk.Frame):
         self.config_data = Model.open_json(CONFIG_JSON_PATH)
         self.messaging = self.config_data["post_messaging"]
 
-        self.messaging_toggle = tk.Button(self, command=self.toggle_message)
+        self.body = tk.Frame(self)
+        self.body.pack()
+
+        self.messaging_toggle = tk.Button(self.body, command=self.toggle_message)
         self.messaging_toggle.pack()
+
+        self.edit_messaging = None
 
         self.set_messaging_toggle()
     
@@ -49,13 +55,81 @@ class ConfigureMessaging(tk.Frame):
         self.config_data["post_messaging"] = self.messaging
         Model.write_json_file(CONFIG_JSON_PATH, self.config_data)
         self.set_messaging_toggle()
-        pass
+
 
     def set_messaging_toggle(self):
         if self.messaging:
             self.messaging_toggle.config(text="Disable Messaging",
                                          bg="#72c5c0")
+            self.edit_messaging_group = tk.Frame(self.body)
+            self.edit_messaging_group.pack()
+            self.edit_messaging = EditMessaging(self.edit_messaging_group, self.config_data)
+            self.edit_messaging.pack()
         else: 
             self.messaging_toggle.config(text="Enable Messaging",
                                          bg="grey")
-        self.messaging_toggle.pack()
+            if self.edit_messaging:
+                self.edit_messaging_group.pack_forget()
+
+class EditMessaging(tk.Frame):
+    def __init__(self, container, config_data):
+        super().__init__(container)
+        self.config_data = config_data
+
+        #Grouper Frame
+        self.body = tk.Frame(container)
+        self.body.columnconfigure(0,weight=1)
+        self.body.columnconfigure(1, weight=1)
+        self.body.columnconfigure(2, weight=1)
+        self.body.pack()
+
+
+        self.email_lbl = tk.Label(self.body, text="Email: ")
+        self.email_lbl.grid(column=0, row=0)
+
+        self.email_field = TextField(self.body,1)
+        self.email_field.grid(column=1,row=0)
+        self.init_email_field()
+
+        self.update_btn = tk.Button(self.body, text= "Update", command=self.update_email)
+        self.update_btn.grid(column=2, row=0)
+
+
+    
+    def init_email_field(self):
+        email = self.config_data["email"]
+        if email:
+            self.email_field.set_text(email)
+        pass
+
+    def update_email(self):
+        new_email = self.email_field.get_text()
+        if is_email(new_email):
+            
+            EmailValidation(self)
+        elif is_hashed_email(new_email):
+            pass
+        else:
+            showinfo(title="Error", message="Error input email or hashed email.")
+        pass
+
+class EmailValidation():
+    def __init__(self, master):
+        new_window = tk.Toplevel(master)
+        new_window.title("New Window")
+        new_window.geometry("300x600")
+        label = tk.Label(new_window, text="This is a new window!")
+        label.pack(padx=20, pady=20)
+        new_window.grab_set()
+        
+
+def is_email(string:str) -> bool:
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return bool(re.match(pattern, string))
+
+def is_hashed_email(string:str) -> bool:
+    hashed = False
+    pattern = r'[a-zA-Z0-9]'
+    if (re.match(pattern, string) and len(string) > 30 and len(string) < 34):
+        hashed = True
+    return hashed
